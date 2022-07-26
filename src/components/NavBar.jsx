ec3faef5b43ec4bba82bd054229cdadd9a45d1a6
woodsjd11 <woodsjd@hotmail.co.uk>
2022-07-26 08:46:33 +0100

    add ability to tab between topics in the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,30 @@
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function NavBar({ setCurrentTopic }) {
-  let navigate = useNavigate()
-
-  function handleChange(value) {
-    navigate(`/topics/${value}`);
-  }
   return (
-    <nav>
+    <nav className="navbar">
       <Link to="/">All Articles</Link>
-      <label htmlFor="topics">Select Topic: </label>
-      <select
-        name="topics"
-        onChange={(e) => {
-          handleChange(e.target.value)
-          setCurrentTopic(e.target.value);
-        }}
-      >
-        <option value="coding">Coding</option>
-        <option value="football">Football</option>
-        <option value="cooking">Cooking</option>
-      </select>
+      <div className="dropdown">
+        <button className="dropbtn" tabIndex="0">
+          Select Topic
+        </button>
+        <div className="dropdown-content">
+          <Link to="/topics/coding">Coding</Link>
+          <Link to="/topics/cooking">Cooking</Link>
+          <Link to="/topics/football">Football</Link>
+        </div>
+      </div>
+      <div className="sortby">
       <label htmlFor="sortby">Sort By: </label>
-      <select name="sortby">
-        <option value="titleasc">Title (A-Z)</option>
-        <option value="titledesc">Title (Z-A)</option>
-        <option value="pubdateasc">Publication Date (old to new)</option>
-        <option value="pubdatedesc">Publication Date (new to old)</option>
-        <option value="votesasc">Votes (high to low)</option>
-        <option value="votesdesc">Votes (low to high)</option>
-      </select>
+        <select name="sortby">
+          <option value="titleasc">Title (A-Z)</option>
+          <option value="titledesc">Title (Z-A)</option>
+          <option value="pubdateasc">Publication Date (old to new)</option>
+          <option value="pubdatedesc">Publication Date (new to old)</option>
+          <option value="votesasc">Votes (high to low)</option>
+          <option value="votesdesc">Votes (low to high)</option>
+        </select>
+      </div>
     </nav>
   );
 }
