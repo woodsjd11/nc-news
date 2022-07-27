c0a8bc393ec3ee4d69b9bc77018cc1caad8062a6
woodsjd11 <woodsjd@hotmail.co.uk>
2022-07-27 10:10:13 +0100

    add functional view/hide comments button

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -4,6 +4,7 @@ import * as api from "../api-calls/api-get";
 export default function Comments({ currentArticle, id }) {
   const [comments, setComments] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
@@ -15,33 +16,44 @@ export default function Comments({ currentArticle, id }) {
 
   return (
     <div>
-      <button value={currentArticle.comment_count}>
-        View Comments ({currentArticle.comment_count})
+      <button
+        value={currentArticle.comment_count}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        {!isOpen ? "View Comments" : "Hide Comments"} (
+        {currentArticle.comment_count})
       </button>
       {isLoading ? (
         <p>Loading Comments</p>
       ) : (
-        <ul style={{ listStyle: "none" }}>
-          {comments.map((comment) => {
-            const commentDate = comment.created_at;
-            // provides same result on re-render
-            const formattedDate = /^\d{4}/.test(commentDate)
-              ? commentDate.split("T")[0].split("-").reverse().join("-")
-              : commentDate;
-            console.log(formattedDate);
-            comment.created_at = formattedDate;
-            return (
-              <li key={comment.comment_id}>
-                <p>
-                  <u>
-                    {comment.author} {comment.created_at}
-                  </u>
-                </p>
-                <p>{comment.body}</p>
-              </li>
-            );
-          })}
-        </ul>
+        <>
+          {isOpen && (
+            <div>
+              <ul style={{ listStyle: "none" }}>
+                {comments.map((comment) => {
+                  const commentDate = comment.created_at;
+                  // provides same result on re-render
+                  const formattedDate = /^\d{4}/.test(commentDate)
+                    ? commentDate.split("T")[0].split("-").reverse().join("-")
+                    : commentDate;
+                  comment.created_at = formattedDate;
+                  return (
+                    <li key={comment.comment_id}>
+                      <p>
+                        <u>
+                          {comment.author} {comment.created_at}
+                        </u>
+                      </p>
+                      <p>{comment.body}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
